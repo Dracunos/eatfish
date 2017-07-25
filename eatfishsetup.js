641a319b3c390f73bda15199ddcc415efb16e5c0
Dracunos <markalexander72@gmail.com>
2017-07-25 11:49:58 -0700

    Cleaned up a bit more code

--- eatfishsetup.js
+++ eatfishsetup.js
@@ -2,5 +2,5 @@ var ECS = {};
 var canvas = document.getElementById("display");
 
 function checkVal(val, funcName) {
-    if (val == undefined) {throw funcName + " given no value";}
+    if (val === undefined) {throw funcName + " given no value";}
 }
