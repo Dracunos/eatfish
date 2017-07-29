0b408861de37bde2a4b3f2bfef04ba3bf449b5f2
Dracunos <dracunos72@hotmail.com>
2017-07-28 19:26:19 -0700

    Disabled arrow key scrolling of web page

--- eatfishsystems.js
+++ eatfishsystems.js
@@ -117,6 +117,9 @@ ECS.Systems.Input = function systemInput(entities) {
 ECS.Systems.Input.keys = [];
 
 window.addEventListener('keydown', function (e) {
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
     ECS.Systems.Input.keys = (ECS.Systems.Input.keys || []);
     ECS.Systems.Input.keys[e.keyCode] = (e.type == "keydown");
 });
